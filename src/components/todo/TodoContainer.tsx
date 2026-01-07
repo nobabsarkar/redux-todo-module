3978f62be5f1af824fb0f1d402b35fc83ca4ce36
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2026-01-08 00:13:14 +0600

    create redux todo app

---
+++ src/components/todo/TodoContainer.tsx
@@ -0,0 +1,31 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
+import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
+
+const TodoContainer = () => {
+  return (
+    <div>
+      <div className="flex justify-between mb-3">
+        <AddTodoModal />
+        <Button className="bg-primary-gradient text-xl font-semibold">
+          Filter
+        </Button>
+        <TodoFilter />
+      </div>
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
+        {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
+          <p>There is no task pending</p>
+        </div> */}
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3 ">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoContainer;
---
+++ src/components/todo/TodoFilter.tsx
@@ -0,0 +1,53 @@
+import { type DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+type Checked = DropdownMenuCheckboxItemProps["checked"];
+
+const TodoFilter = () => {
+  const [showStatusBar, setShowStatusBar] = useState<Checked>(true);
+  const [showActivityBar, setShowActivityBar] = useState<Checked>(false);
+  const [showPanel, setShowPanel] = useState<Checked>(false);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="bg-primary-gradient text-xl font-semibold">
+          Filter
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={showStatusBar}
+          onCheckedChange={setShowStatusBar}
+        >
+          Status Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showActivityBar}
+          onCheckedChange={setShowActivityBar}
+          disabled
+        >
+          Activity Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showPanel}
+          onCheckedChange={setShowPanel}
+        >
+          Panel
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default TodoFilter;
